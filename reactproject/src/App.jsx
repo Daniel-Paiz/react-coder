892fa56cad7236762061ee216b439f2258a74eda
parce <night.paiz@gmail.com>
2022-09-08 18:26:20 -0300

    subir-proyecto

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from "./Componentes./NavBar";
+
+
+const App = () => {
+  return (
+    <NavBar />
+  )
+}
+
+export default App;
